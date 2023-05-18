2a76746040d8a2de087f9f372b3c3dc1bb8cba71
Furqan A. Kassa <kassafur@uw.edu>
2023-05-18 16:58:24 -0700

    45% accuracy now, more features inserted

--- main.py
+++ main.py
@@ -9,17 +9,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from flask import Flask, render_template, url_for, request, redirect
 
-# Notes
-"""
-For audio 54 in the Jazz genre, the file was corrupted so I deleted it.
-For the blues genre, 1-7 files were missing as well as the 100th song.
-Still need to add on the other 19 MFCC features.
-"""
-
-# relative path to the genre directory
-path = "audio_music/genres"
-music_df = pd.DataFrame() # global access to the dataframe
-
 
 # Model work
 def extract_features() -> None:
@@ -27,33 +16,59 @@ def extract_features() -> None:
     Loops through the genre directory and extracts the MFCC features
     from the audio files and builds a dataframe out of those features. 
     """
-    global music_df
+    path = "audio_music/genres"
     features = []
     label = ["blues", "classical", "country", "disco",
-             "hiphop", "jazz", "metal", "pop", "reggae", 
+             "hiphop", "jazz", "metal", "pop", "reggae",
              "rock"]
 
     for idx, genre in enumerate(os.listdir(path)):
         genre_path = path + '/' + genre + '/'
         for audio_file in os.listdir(genre_path):
-            curr_mfcc = calculate_mfcc(genre_path + audio_file)
-            features.append([label[idx], curr_mfcc])
-    
-    music_df = pd.DataFrame(features, columns=["label", "feature"])
+            extracted_feat = build_features(genre_path + audio_file)
+            curr_mfcc = [mfcc for mfcc in extracted_feat[-1]]
+            features.append([label[idx]] + [extracted_feat[0]] + [extracted_feat[1]]
+                                                               + [extracted_feat[2]] + curr_mfcc)
+
+    columns = ["Genre", "Centroid", "Bandwith", "Chroma"] + [f"MFCC-{i + 1}" for i in range(20)]
+    df = pd.DataFrame(features, columns=columns)
+    train_model(df)
 
 
-def calculate_mfcc(file_path: str) -> list[float]:
+def build_features(file_path: str) -> list[float]:
     """
     Loads the audio file and retrieves the signal and sample rate
-    to calculate the MFCC feature of each audio file. Returns
-    a list of the mean MFCC values acorss the time axis.
+    to calculate the MFCC, centroid, bandwidth, and chroma features
+    of each audio file. Returns a list of the mean values of the features.
     """
     signal, sample_rate = lib.load(file_path)
+    centr = lib.feature.spectral_centroid(y=signal, sr=sample_rate)
+    bandw = lib.feature.spectral_bandwidth(y=signal, sr=sample_rate)
+    chroma = lib.feature.chroma_cens(y=signal, sr=sample_rate)
     mfccs = lib.feature.mfcc(y=signal, sr=sample_rate, n_mfcc=20)
-    return np.mean(mfccs.T, axis=0)
+    return [np.mean(centr), np.mean(bandw), np.mean(chroma), np.mean(mfccs.T, axis=0)]
 
 
-def select_file(file_path: str) -> None | str:
+def train_model(data: pd.DataFrame) -> float:
+    """
+    Passes in the datframe and trains the model to predict
+    the genre by utilizing the extracted features. Split the
+    data for testing/training and returns an accuracy score.
+    """
+    features = data.loc[:, "Centroid":"MFCC-20"]
+    label = data["Genre"]
+
+    features_train, features_test, label_train, label_test = \
+                    train_test_split(features, label, test_size=0.2)
+    
+    model = DecisionTreeClassifier()
+    model.fit(features_train, label_train)
+    
+    prediction = model.predict(features_test)
+    return accuracy_score(label_test, prediction)
+
+
+def select_file(file_path: str) -> None:
     """
     Extracts the features from the audio file uploaded by the user
     and uses the trained model to make a predicted on the extracted
@@ -61,33 +76,10 @@ def select_file(file_path: str) -> None | str:
     """
     if (not file_path):
         return None
-    
+    pass
     # Extract features from the selected file
     # features = extract_features(file_path)
     # label = model.predict([features])[0]
-
-def train_model(data: pd.DataFrame) -> str:
-    """
-    Only because we have 1 feature rn, TESTING purporses.
-    Training the model, and making predictions on the genres.
-    Current accuracy score: 19% (need more features)
-    """
-    global music_df
-    features = music_df.loc[:, "MFCC-1"]
-    features = features.values.reshape(-1, 1)
-    label = music_df["Genre-Names"]
-
-    """
-    features_train, features_test, label_train, label_test =
-                    train_test_split(features, label, test_size=0.2)
-    """
-    
-    model = DecisionTreeClassifier()
-    model.fit(features, label)
-    
-    data = np.reshape(data, (-1, 1))
-    prediction = model.predict(data)
-    return prediction[-1]
 
 
 def main():
@@ -132,7 +124,7 @@ def calculate(file_name: str):
     from the uploaded audio and a prediction will be made. The accuracy score
     will be displayed as well as the name of genre classified by the model.
     """
-    frequency = select_file("audio_music/uploads/" + file_name)
+    frequency = "" """select_file("audio_music/uploads/" + file_name)"""
     genre = train_model(frequency)
     # os.remove(file_name) remove file from server after getting genre
     return render_template("results.html", outcome=f"Genre: {genre}")
